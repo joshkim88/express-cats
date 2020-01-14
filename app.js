28b7eef9162a36ce3582dee5d9c4b18cff8d5765
joshua kim <j0shxone@gmail.com>
2020-01-14 15:53:15 -0500

    2nd commit try.

---
+++ app.js
@@ -0,0 +1,9 @@
+var cat = require("cat-me");
+var knockjoke = require("knock-knock-jokes");
+
+
+console.log(cat());
+console.log("================");
+console.log(knockjoke());
+console.log("================");
+
